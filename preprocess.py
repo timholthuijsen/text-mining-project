091b862defee947d16aade6610b336ee894cf4df
Berke Filiz <edu.berkefiliz@gmail.com>
2021-04-18 18:52:23 +0200

    Added pos-tag and lemmatize functions

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,66 @@
 # -*- coding: utf-8 -*-
 
+import nltk
+from nltk.corpus import wordnet as wn
+
 import clean_data as cd
 
+
+MAP = {"VERB" : wn.VERB, "NOUN" : wn.NOUN, "ADJ" : wn.ADJ, "ADV" : wn.ADV}
+
+
+def pos_tag_db(db: list) -> list:
+    """
+    PoS-Tag a given database
+
+    Parameters
+    ----------
+    db : list
+        Database to PoS-Tag.
+
+    Returns
+    -------
+    list
+        PoS-Tagged database.
+
+    """
+    return [nltk.pos_tag(sentence.split(), tagset = "universal") for sentence in db]
+        
+
+def lemmatize_db(db: list, exclude: list = []) -> list:
+    """
+    Lemmatize given database
+
+    Parameters
+    ----------
+    db : list
+        PoS-Tagged dataset to lemmatize.
+    exclude : list, optional
+        Types to exclude from the output. The default is [].
+
+    Returns
+    -------
+    list
+        Lemmatized database.
+
+    """
+    lemmatized_db = []
+    for sentence in db:
+        lemmatized_sentence = []
+        for w, p in sentence:
+            if p in exclude:
+                continue
+            elif p in MAP.keys():
+                lemma = nltk.WordNetLemmatizer().lemmatize(w, pos = MAP[p])
+            else:
+                lemma = nltk.WordNetLemmatizer().lemmatize(w)
+            lemmatized_sentence.append((lemma, p))
+        lemmatized_db.append(lemmatized_sentence)
+    return lemmatized_db
+    
+
+# EXAMPLE USE:
+    
 # Get a list of available databases
 dbs = cd.getDatabases()
 
@@ -10,3 +69,10 @@ clean_db = cd.cleanFile(dbs[0])
 
 # Print 10 lines
 cd.head(clean_db, 10)
+
+# PoS-Tag and lemmatize
+postagged = pos_tag_db(clean_db)
+lemmatized = lemmatize_db(postagged, [".", "X"])
+
+# Print 5 lines from the result
+cd.head(lemmatized)
